6fb1619ea7645c06b9ba592f2f443e833c3503ce
Prijic, Nikola <nikola.prijic@nttdata.com>
2024-01-11 12:38:12 +0100

    fix:  removing unnecessary code from Vite config

--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,22 +1,5 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   base: "./",
-//   resolve: {
-//     alias: {
-//       // If you have a specific entry file for Vue, replace it here.
-//       vue: "vue/dist/vue.esm-bundler.js",
-//     },
-//     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
-//   },
-//   build: {
-//     sourcemap: true,
-//   },
-// });
-
 export default defineConfig({
   plugins: [vue()],
   base: "./",
